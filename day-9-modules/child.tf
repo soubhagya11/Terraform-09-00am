5aae2583cd1a24347c5aa0895ba320211d9ea452
soubhagya11 <168725707+soubhagya11@users.noreply.github.com>
2024-07-18 07:46:18 +0530

    Update child.tf

--- day-9-modules/child.tf
+++ day-9-modules/child.tf
@@ -1,8 +1,1 @@
-module "test" {
-    source = "./module" 
-    ami       	= "ami-0440d3b780d96b29d"
-    instance_type = "t2.micro"
-    subnet_id 	= "subnet-0f2a81412b51a8ae0"
-    key_name  	= "testlt"
-    name        = "test_ec2"
-}
+
