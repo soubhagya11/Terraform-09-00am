59baa5661945af90630989f0c507ba7dd5f9063c
soubhagya11 <168725707+soubhagya11@users.noreply.github.com>
2024-09-11 07:12:49 +0530

    Update main.tf

--- day-2-basic_code_ec2/main.tf
+++ day-2-basic_code_ec2/main.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "dev" {
     ami = var.ami_id
     instance_type = var.instance_type
     key_name = var.key_name
-    associate_public_ip_address = true
     tags = {
       Name = "dev_ec2"
     }
